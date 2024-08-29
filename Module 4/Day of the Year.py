cbacc3da748bdddf301e867b91a395124ce7a93c
Kamivision <54725955+Kamivision@users.noreply.github.com>
2024-08-28 19:54:06 -0500

    Update Files

---
+++ Module 4/Day of the Year.py
@@ -0,0 +1,28 @@
+# Objectives
+# Familiarize the student with:
+
+# projecting and writing parameterized functions;
+# utilizing the return statement;
+# building a set of utility functions;
+# utilizing the student's own functions.
+# Scenario
+# Your task is to write and test a function which takes three arguments (a year, a month, and a day of the month) and returns the corresponding day of the year, or returns None if any of the arguments is invalid.
+
+# Use the previously written and tested functions. Add some test cases to the code. This test is only a beginning.
+
+def is_year_leap(year):
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+#
+# Your code from LAB 4.3.1.7.
+#
+
+def day_of_year(year, month, day):
+#
+# Write your new code here.
+#
+
+print(day_of_year(2000, 12, 31))
