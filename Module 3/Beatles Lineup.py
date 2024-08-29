d232f6f62967b32d810ca6af454d601940a54ca6
Kamivision <54725955+Kamivision@users.noreply.github.com>
2024-08-29 17:15:44 -0500

    Update Beatles Lineup

--- Module 3/Beatles Lineup.py
+++ Module 3/Beatles Lineup.py
@@ -6,8 +6,24 @@
 # Write a program that reflects these changes and lets you practice with the concept of lists. Your task is to:
 
 # step 1: create an empty list named beatles;
-# step 2: use the append() method to add the following members of the band to the list: John Lennon, Paul McCartney, and George Harrison;
-# step 3: use the for loop and the append() method to prompt the user to add the following members of the band to the list: Stu Sutcliffe, and Pete Best;
-# step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
-# step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+beatles = [ ]
 
+# step 2: use the append() method to add the following members of the band to the list: John Lennon, Paul McCartney, and George Harrison;
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print(beatles)
+
+# step 3: use the for loop and the append() method to prompt the user to add the following members of the band to the list: Stu Sutcliffe, and Pete Best;
+for member in range(2):
+    member = input("Enter a new member: ")
+    beatles.append(member)
+print(beatles)
+
+# step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
+del beatles[3: ]
+print(beatles)
+
+# step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+beatles.insert(0, "Ringo Starr")
+print(beatles)
